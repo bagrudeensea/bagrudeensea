2ba9e43a26dd43d6b92afe8bd0300a36bc1c694f
bagrudeensea <35652697+bagrudeensea@users.noreply.github.com>
2018-01-30 16:13:54 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a;
+scanf("%d",&n);
+for(a=2,a<n,a++)
+{
+ if(n%a==0)
+ {
+ printf("no');
+ break;
+ }
+ }
+ if(a==n)
+ printf("yes");
+ break;
+ }}

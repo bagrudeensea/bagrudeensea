7e972d5247bf93b0f94f137c1b902efb21393a4f
bagrudeensea <35652697+bagrudeensea@users.noreply.github.com>
2018-02-06 16:02:36 +0530

    Create 21.c

---
+++ 21.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,c=1,d,sum=0,a;
+clrscr();
+printf("Enter i and d values");
+scanf("%d %d ",&n,&d);
+for(a=1;a<=n;a++)
+{
+sum=sum+c;
+c=c+d;
+}
+printf("\n%d",sum);
+return 0;
+}
+void main()
+{

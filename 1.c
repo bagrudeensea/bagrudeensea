edc65b15ad207e372c6ee1809ed9c7df0cf15739
bagrudeensea <35652697+bagrudeensea@users.noreply.github.com>
2018-02-08 15:56:39 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cin>>i;
+    if(i>0)
+    {
+        cout<<"postive";
+    }
+    else if(i<0)
+    {
+        cout<<"Negative";
+    }
+    else
+    {
+        cout<<"Given number is zero";
+    }
+    return 0;
+}
+

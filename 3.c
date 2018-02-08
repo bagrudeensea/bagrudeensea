8bcf3d1341c9c02c238e1b0c49701aa2db039e05
bagrudeensea <35652697+bagrudeensea@users.noreply.github.com>
2018-02-08 16:10:42 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+    char f;
+    g=getchar();
+    
+    if(f=='a'|f=='e'|f=='i'|f=='o'|f=='u'){
+        printf("Vowel");
+    }else {
+        printf("Consonent");
+    }
+}

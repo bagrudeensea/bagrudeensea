37dcfbf230440a0dc927253dd7cd895658201737
bagrudeensea <35652697+bagrudeensea@users.noreply.github.com>
2018-02-13 15:36:41 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a[0],a[1],a[2];
+printf("\nenter the array of elements");
+scanf("%d%d%d",&a[0],a[1],a[2]);
+a[0]=1;
+a[1]=2;
+a[2]=3;
+{
+printf("\nenter the array of elements with its index");
+}
+}

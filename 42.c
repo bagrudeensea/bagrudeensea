205234a3919e6549055c3435c71b1b537ebaef25
bagrudeensea <35652697+bagrudeensea@users.noreply.github.com>
2018-03-15 16:04:15 +0530

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+   char s1[1000],s2[1000];
+   int i,j,l1,l2,flag;
+   scanf("%s %s",s1,s2);
+   l1=strlen(s1);
+   l2=strlen(s2);
+   if(l1==l2)
+   {
+   for(i=0;i<l1;i++)
+   {
+      for(j=0;j<l2;j++)
+      {
+          if(s1[i]>s2[j])
+          flag=1;
+          else if(s1[i]<s2[j])
+          flag=2;
+          else
+          flag=3;
+      }
+      
+   }
+   }
+   if(flag==1)
+   {
+   for(i=0;i<l1;i++)
+   printf("%s",s1[i]);
+   }
+   else if(flag==2)
+   {
+       for(j=0;j<l2;j++)
+   
+   printf("\n%s",s2[j]);
+   }
+   
+   else
+   for(i=0;i<l1;i++)
+   {
+       for(j=0;j<l2;j++)
+       {
+   
+   printf("\n%s %s",s1[i],s2[j]);
+       }
+   }
+   return 0;
+}
+   
+  

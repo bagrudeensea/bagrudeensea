9a91438a480ca17f6693e259a8cb811ec93ed67d
bagrudeensea <35652697+bagrudeensea@users.noreply.github.com>
2018-03-15 16:19:01 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+            void main()
+            {
+            	int n=100;
+            	if(n%2==0)
+            	{
+     
+            	printf("\nyes");
+     
+            	}
+            	else
+            	{
+            		printf("\nno");
+            	}
+            }

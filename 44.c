f881f59aed2448749f0c7b972d5ae9eb4d020b3c
bagrudeensea <35652697+bagrudeensea@users.noreply.github.com>
2018-03-15 16:07:48 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+    void main()
+    {
+    	int n=7;
+    	if(n=!0&&n<=11)
+    	{
+    		printf("\n1 to 10");
+    	}
+    	else
+    	{
+    		printf("\nnot 1 to 10");
+    	}
+    }
+     

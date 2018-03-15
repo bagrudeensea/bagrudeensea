fef7b0d1ab479e98c262d1331e61714c429dd178
bagrudeensea <35652697+bagrudeensea@users.noreply.github.com>
2018-03-15 16:11:21 +0530

    Create 46.c

---
+++ 46.c
@@ -0,0 +1,11 @@
+  #include<stdio.h>
+            void main()
+            {
+            	int n=100,s,i;
+            	for(i=0;i<n+1;i++)
+            	{
+            	//	s=n+1;
+            		printf("\n%d",i);
+            	}
+            }
+     
